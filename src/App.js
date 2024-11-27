118eb98535f7f1485216150dc80e03c407fd50be
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-27 17:18:14 +0530

    Footer completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
-import './App.css';
 import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from "./Components/NavBar/NavBar";
+
 
 
 function App() {
   return (
     <div >
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import { Nav, Container } from "react-bootstrap";
+
+const Footer = (props) => {
+
+    return (
+        <footer className="bg-info">
+
+            <Container className="text-auto">
+                <h1 className="text-white display-1"> THE GENERICS</h1>
+                <Nav className="mx-auto text-center fw-bold">
+
+
+
+                    <Nav.Link href="https://www.youtube.com" >
+                        <img width="30px" src="/images/youtube-logo.jpg" alt="youtube link" />
+                    </Nav.Link>
+                    <Nav.Link href="https://spotify.com" >
+                        <img width="30px" src="/images/Spotify Logo.png" alt="spotify link" />
+                    </Nav.Link>
+                    <Nav.Link href="https://facebook.com" >
+                        <img width="30px" src="/images/Facebook Logo.png" alt="facebook link" />
+                    </Nav.Link>
+
+
+                </Nav>
+            </Container>
+
+        </footer>
+    )
+
+}
+
+export default Footer;
